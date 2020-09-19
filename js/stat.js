8b647e5ed777a78cd8eb9e01f35369a15a0f0ed8
Dima Os <dimaos@protonmail.com>
2020-09-19 16:08:25 +0300

    Реализует вывод поздравительно-мотивирующей надписи в случае победы

--- js/stat.js
+++ js/stat.js
@@ -11,4 +11,9 @@ const renderCloud = function (ctx, x, y, color) {
 window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx, 110, 20, `rgba(0, 0, 0, 0.7)`);
   renderCloud(ctx, 100, 10, `#ffffff`);
+
+  ctx.fillStyle =`#000000`;
+  ctx.font =`16px PT Mono`;
+  ctx.fillText(`Ура вы победили!`, 120, 41);
+  ctx.fillText(`Список результатов:`, 120, 57);
 };
