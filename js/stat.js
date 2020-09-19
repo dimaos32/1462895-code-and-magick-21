910f3d023c86f9dc420be7798d88aec06d18c5c5
Dima Os <dimaos@protonmail.com>
2020-09-19 15:39:15 +0300

    Рисует квадратное «облако»

--- js/stat.js
+++ js/stat.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const CLOUD_WIDTH = 420;
+const CLOUD_HEIGHT = 270;
+
+const renderCloud = function (ctx, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+};
+
+window.renderStatistics = function (ctx, names, times) {
+  renderCloud(ctx, 110, 20, `rgba(0, 0, 0, 0.7)`);
+  renderCloud(ctx, 100, 10, `#ffffff`);
+};
