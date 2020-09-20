b1abd5a7479f1bf626b07b04d68c1333bdc268d2
Dima Os <dimaos@protonmail.com>
2020-09-20 15:00:13 +0300

    Рефакторинг кода

--- js/stat.js
+++ js/stat.js
@@ -1,19 +1,32 @@
 'use strict';
 
+const COLOR_CLOUD = `#ffffff`;
+const COLOR_CLOUD_SHADOW = `rgba(0, 0, 0, 0.7)`;
+const COLOR_TEXT = `#000000`;
+const COLOR_CURRENT_PLAYER = `#ff0000`;
+
 const CLOUD_WIDTH = 420;
 const CLOUD_HEIGHT = 270;
 const CLOUD_X = 100;
 const CLOUD_Y = 10;
-const GAP = 50;
+
 const FONT_SIZE = 16;
-const BARS_X = 40;
-const BARS_Y = 85;
-const BAR_WIDTH = 40;
-const BAR_MAX_HEIGHT = 150;
-const NAMES_X = BARS_X;
-const NAMES_Y = 255;
+const FONT_FAMILY = `PT Mono`;
+
 const MESSAGE_X = 20;
 const MESSAGE_Y = 15;
+const MESSAGE_CONTENT = [
+  `Ура вы победили!`,
+  `Список результатов:`
+];
+
+const BAR_WIDTH = 40;
+const BAR_MAX_HEIGHT = 150;
+const GAP = 50;
+const BARS_X = 40;
+const BARS_Y = 85;
+
+const NAMES_Y = 255;
 
 const renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
@@ -32,45 +45,39 @@ const getMaxElement = function (arr) {
   return maxElement;
 };
 
+const getRandomIntNumber = (min = 0, max = 100) => {
+  return min + Math.floor(Math.random() * (max - min + 1));
+};
+
+const ramdomBlueColor = () => {
+  return `hsl(240, ${getRandomIntNumber()}%, 50%)`;
+};
+
 window.renderStatistics = function (ctx, names, times) {
-  let badTime = getMaxElement(times);
+  const badTime = getMaxElement(times);
 
-  renderCloud(ctx, CLOUD_X + 10, CLOUD_Y + 10, `rgba(0, 0, 0, 0.7)`);
-  renderCloud(ctx, CLOUD_X, CLOUD_Y, `#ffffff`);
+  renderCloud(ctx, CLOUD_X + 10, CLOUD_Y + 10, COLOR_CLOUD_SHADOW);
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, COLOR_CLOUD);
 
-  ctx.font = `${FONT_SIZE}px PT Mono`;
+  ctx.font = `${FONT_SIZE}px ` + FONT_FAMILY;
+
+  ctx.fillStyle = COLOR_TEXT;
+
+  MESSAGE_CONTENT.forEach(function (str, i) {
+    const currentStringY = CLOUD_Y + MESSAGE_Y + FONT_SIZE * (i + 1);
+    ctx.fillText(str, CLOUD_X + MESSAGE_X, currentStringY);
+  });
 
   for (let i = 0; i < names.length; i++) {
-    ctx.fillStyle = `#000000`;
-    ctx.fillText(
-        names[i],
-        CLOUD_X + NAMES_X + (GAP + BAR_WIDTH) * i,
-        CLOUD_Y + NAMES_Y
-    );
+    const currentBarX = CLOUD_X + BARS_X + (GAP + BAR_WIDTH) * i;
+    const currentBarY = CLOUD_Y + BARS_Y + BAR_MAX_HEIGHT * (badTime - times[i]) / badTime;
 
-    ctx.fillText(
-        Math.round(times[i]),
-        CLOUD_X + NAMES_X + (GAP + BAR_WIDTH) * i,
-        CLOUD_Y + BARS_Y + BAR_MAX_HEIGHT * (badTime - times[i]) / badTime - 10
-    );
+    ctx.fillStyle = COLOR_TEXT;
+    ctx.fillText(names[i], currentBarX, CLOUD_Y + NAMES_Y);
 
-    ctx.fillStyle = names[i] === `Вы` ? `#ff0000` : `hsl(240, ${Math.ceil(Math.random() * 100)}%, 50%)`;
-    ctx.fillRect(
-        CLOUD_X + BARS_X + (GAP + BAR_WIDTH) * i,
-        CLOUD_Y + BARS_Y + BAR_MAX_HEIGHT * (badTime - times[i]) / badTime,
-        BAR_WIDTH,
-        BAR_MAX_HEIGHT * times[i] / badTime
-    );
+    ctx.fillText(Math.round(times[i]), currentBarX, currentBarY - 10);
+
+    ctx.fillStyle = names[i] === `Вы` ? COLOR_CURRENT_PLAYER : ramdomBlueColor();
+    ctx.fillRect(currentBarX, currentBarY, BAR_WIDTH, BAR_MAX_HEIGHT * times[i] / badTime);
   }
-
-  ctx.fillStyle = `#000000`;
-  ctx.fillText(
-      `Ура вы победили!`,
-      CLOUD_X + MESSAGE_X,
-      CLOUD_Y + MESSAGE_Y + FONT_SIZE
-  );
-  ctx.fillText(`Список результатов:`,
-      CLOUD_X + MESSAGE_X,
-      CLOUD_Y + MESSAGE_Y + FONT_SIZE * 2
-  );
 };
