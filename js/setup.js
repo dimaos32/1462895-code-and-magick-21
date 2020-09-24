a977a2b97d44d1028f03be48b740d9dc32850069
Dima Os <dimaos@protonmail.com>
2020-09-24 13:29:15 +0300

    Модернизирует функцию - теперь она возвращает n случайных элементов массива

--- js/setup.js
+++ js/setup.js
@@ -1,4 +1,13 @@
-const getRandomArrayElement = (arr) => {
-  element = Math.floor(Math.random() * arr.length);
-  return arr[element];
-};
+const getRandomArrayElements = (arr, n = 1) => {
+  let randomArray = [];
+
+  for (let i = 0; i < arr.length && i < n; i++) {
+    const Element = getRandomIntNumber(i, arr.length - 1);
+    randomArray.push(arr[Element]);
+    const swap = arr[Element];
+    arr[Element] = arr[i];
+    arr[i] = swap;
+  }
+
+  return randomArray;
+}
