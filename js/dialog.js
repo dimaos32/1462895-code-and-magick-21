0cf0deeb1815b7f3805a469531880066b56e3f7a
Dima Os <dimaos@protonmail.com>
2020-10-05 02:09:32 +0300

    Добавляет возможность двигать окно настроек

--- js/dialog.js
+++ js/dialog.js
@@ -1,13 +1,25 @@
 'use strict';
 
 (() => {
+
   const MIN_NAME_LENGTH = 2;
   const MAX_NAME_LENGTH = 25;
 
   const setup = document.querySelector(`.setup`);
   const setupOpen = document.querySelector(`.setup-open`);
   const setupClose = setup.querySelector(`.setup-close`);
-  const userNameInput = document.querySelector(`.setup-user-name`);
+  const userNameInput = setup.querySelector(`.setup-user-name`);
+  const dialogHandle = setup.querySelector(`.upload`);
+
+  const popapOffset ={
+    x: 0,
+    y: 0,
+  };
+
+  const resetPopapOffset = () => {
+    popapOffset.x = 0;
+    popapOffset.y = 0;
+  }
 
   const onSetupEscPress = (evt) => {
     if (evt.key === `Escape` && userNameInput !== document.activeElement) {
@@ -23,6 +35,8 @@
 
   const closePopup = () => {
     setup.classList.add(`hidden`);
+    setup.style = `transform: translateX(-50%);`;
+    resetPopapOffset();
     document.removeEventListener(`keydown`, onSetupEscPress);
   };
 
@@ -62,5 +76,57 @@
     }
   });
   userNameInput.addEventListener(`input`, validateUserName);
+
+  dialogHandle.addEventListener(`mousedown`, (evt) => {
+    if (evt.button === 0) {
+      evt.preventDefault();
+
+      const startCoords = {
+        x: evt.clientX - popapOffset.x,
+        y: evt.clientY - popapOffset.y,
+      };
+
+      let isMoved = false;
+
+      const onMouseMove = (moveEvt) => {
+        moveEvt.preventDefault();
+
+        isMoved = true;
+
+        const shift = {
+          x: moveEvt.clientX - startCoords.x,
+          y: moveEvt.clientY - startCoords.y,
+        };
+
+        setup.style = `transform: translate(calc(-50% + ${shift.x}px), ${shift.y}px)`
+
+        popapOffset.x = shift.x;
+        popapOffset.y = shift.y;
+      };
+
+      const onMouseUp = (upEvt) => {
+        upEvt.preventDefault();
+
+        document.removeEventListener(`mousemove`, onMouseMove);
+        document.removeEventListener(`mouseup`, onMouseUp);
+
+        startCoords.x = upEvt.clientX;
+        startCoords.y = upEvt.clientY;
+
+        if (isMoved) {
+          const onDialogHandleClick = (clickEvt) => {
+            clickEvt.preventDefault();
+            dialogHandle.removeEventListener(`click`, onDialogHandleClick);
+          };
+
+          dialogHandle.addEventListener(`click`, onDialogHandleClick);
+        }
+      };
+
+      document.addEventListener(`mousemove`, onMouseMove);
+      document.addEventListener(`mouseup`, onMouseUp);
+    }
+  });
+
 })();
 
